486c1580109340dd7c1855940c6860a7db78e66c
Behnam <38032617+Behnam-Asadi@users.noreply.github.com>
2023-10-09 18:59:03 -0400

    Add files via upload

--- data_processing.py
+++ data_processing.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ast
 
-
+# Function to process og, arith_baseline, and gz data from stats1000.json
 def process_og_gz_arith_baseline():
     with open("stats1000.json") as read_file:
         data = json.load(read_file)
@@ -17,7 +17,7 @@ def process_og_gz_arith_baseline():
                 gz.append(art[2])
     return np.array(og), np.array(arith_baseline), np.array(gz)
 
-
+# Function to process og, gz, neural_varint, and neural_arith data from stats1000.txt
 def process_stats_1000():
     og, gz, neural_varint, neural_arith = [], [], [], []
     with open("stats1000.txt", "r") as f:
@@ -31,7 +31,7 @@ def process_stats_1000():
                 neural_arith.append(t[3])
     return np.array(og), np.array(gz), np.array(neural_varint), np.array(neural_arith)
 
-
+# Function to process neural_arith data from saving_progress.txt
 def process_arith_neural():
     neural_arith = []
     with open("saving_progress.txt", "r") as f:
@@ -42,21 +42,20 @@ def process_arith_neural():
     neural_arith = np.array(neural_arith)
     return neural_arith
 
-
+# Function to get cropped_og data from cropped_arts.json
 def get_cropped_og():
     with open("cropped_arts.json") as read_file:
         data = json.load(read_file)
         return np.array(data)
 
-
+# Function to calculate compression ratios
 def get_compression_ratio(neural_varint, og, neural_arith, arith_baseline):
-    # gz_baseline = og / gz_baseline
     neural_varint = og / neural_varint
     arith_baseline = og / arith_baseline
     neural_arith = og / neural_arith
     return neural_varint, neural_arith, arith_baseline
 
-
+# Function to graph the compression ratios
 def graph_data(gz_baseline, neural_varint, neural_arith, arith_baseline):
     plt.hist(gz_baseline, color='y', label='GZIP_Baseline', alpha=0.7)
     plt.hist(arith_baseline, color='g', label='Arith_Baseline', alpha=0.7)
@@ -67,7 +66,7 @@ def graph_data(gz_baseline, neural_varint, neural_arith, arith_baseline):
     plt.legend()
     plt.show()
 
-
+# Main execution starts here
 og, gz_baseline, neural_varint, neural_arith = process_stats_1000()
 gz_baseline = og / gz_baseline
 og = get_cropped_og()
@@ -75,6 +74,7 @@ og = get_cropped_og()
 og1, arith_baseline, gz_baseline2 = process_og_gz_arith_baseline()
 neural_varint, neural_arith, arith_baseline = get_compression_ratio(neural_varint, og, neural_arith, arith_baseline)
 
+# Printing summary statistics for each method
 print("GZIP mean, min, max, std: ", np.mean(gz_baseline), np.min(gz_baseline), np.max(gz_baseline), np.std(gz_baseline))
 print("Arith baseline mean, min, max, std: ", np.mean(arith_baseline), np.min(arith_baseline), np.max(arith_baseline),
       np.std(arith_baseline))
@@ -83,4 +83,5 @@ print("Neural varint mean, min, max, std: ", np.mean(neural_varint), np.min(neur
 print("Neural airth mean, min, max, std: ", np.mean(neural_arith), np.min(neural_arith), np.max(neural_arith),
       np.std(neural_arith))
 
+# Plotting the data
 graph_data(gz_baseline, neural_varint, neural_arith, arith_baseline)
